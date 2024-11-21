fac209761c39628e973452ad1aae6e1e0188a217
Guilherme Dias <guidias200220@gmail.com>
2024-11-21 11:58:42 -0300

    feat: urlData object created

---
+++ src/main/java/com/gcd/createUrlShortner/UrlData.java
@@ -0,0 +1,15 @@
+package com.gcd.createUrlShortner;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Setter
+@Getter
+public class UrlData {
+
+    private String originalUrl;
+    private long expirationTime;
+}
